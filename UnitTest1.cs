ec2ccb3a4a4c35e94fd564f4b2476c6a35a3c0b4
Jrangel29 <Jrangel29@ua.pt>
2024-01-31 22:24:40 +0000

    update2

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,3 +1,8 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+
 namespace Training;
 
 public class Tests
@@ -8,8 +13,57 @@ public class Tests
     }
 
     [Test]
-    public void Test1()
+    public async Task TextBoxTest()
     {
-        Assert.Pass();
+        //using elimina a instancia de playwright quando acaba
+        using var playwright = await Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions{
+            Headless = false
+        });
+        var page = await browser.NewPageAsync();
+        //Preciso usar estes awaits todos?
+        await page.GotoAsync("https://demoqa.com/");
+        await page.ClickAsync("text=Elements");
+        await page.ClickAsync("text=Text Box");
+        await page.FillAsync("#userName", "Jose Maria");
+        await page.FillAsync("#userEmail", "maria@gmail.com");
+        await page.FillAsync("#currentAddress", "rua do teste, 444");
+        await page.FillAsync("#permanentAddress", "teste da rua, 333"); 
+        await page.ClickAsync("text=Submit");
+    }
+
+    [Test]
+    public async Task RadioButtonTest()
+    {
+        //using elimina a instancia de playwright quando acaba
+        using var playwright = await Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions{
+            Headless = false
+        });
+        var page = await browser.NewPageAsync();
+        await page.GotoAsync("https://demoqa.com/");
+        await page.ClickAsync("text=Elements");
+        await page.ClickAsync("text=Radio Button");
+        await page.GetByLabel("Yes").CheckAsync();
+    }
+
+    [Test]
+    public async Task ButtonsTest()
+    {
+        //using elimina a instancia de playwright quando acaba
+        using var playwright = await Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions{
+            Headless = false
+        });
+        var page = await browser.NewPageAsync();
+        await page.GotoAsync("https://demoqa.com/");
+        await page.ClickAsync("text=Elements");
+        await page.ClickAsync("text=Buttons");
+        await page.DblClickAsync("#doubleClickBtn");
+        await page.IsVisibleAsync("#doubleClickMessage");
+        await page.GetByText("Right Click Me").ClickAsync(new() { Button = MouseButton.Right });
+        await page.IsVisibleAsync("#rightClickMessage");
+        await page.GetByText("Click Me", new() { Exact = true }).ClickAsync();
+        await page.IsVisibleAsync("#dynamicClickMessage");
     }
 }
